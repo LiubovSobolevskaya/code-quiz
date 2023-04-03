751cf4abd5599d65b16a346487663e5011aafc43
Liubov Sobolevskaya <luba@Liubovs-MBP-2.attlocal.net>
2023-04-03 09:00:13 -0700

    add two eventlisteners

--- assets/script.js
+++ assets/script.js
@@ -1,80 +1,110 @@
-var questons;
-
-fetch('assets/questions.json')
-  .then(response => response.json())
-  .then(data => { 
-    questons = data;
-    console.log(data);})
-  .catch(error => console.error(error));
-
-
-
-var questionCounter = 0
-var storedScores =  document.querySelector(".scores");
-var startButton = document.querySelector(".start-quiz");
-var timer = document.querySelector(".timer");
-var secondsLeft = 60;
-var board = document.querySelector(".main-area");
-
-function startTheQuiz(event){
-    event.preventDefault();
-    
-    board.innerHTML = "";
-    displayQuestion();
-
+async function fetchData() {
+    try {
+      const response = await fetch('assets/questions.json');
+      const data = await response.json();
+      processData(data)
+    } catch (error) {
+      console.error(error);
+    }
 }
+  
+function processData(questions) {
 
-var correct_index;
-var answers;
+    var totalQuestions = questions.length;
+    var questionsAnswered = [];
+    for (var i=0; i<totalQuestions; i++){
+        questionsAnswered.push(false);
+    }
+    var questionCounter = -1;
+    var storedScores =  document.querySelector(".scores");
+    var startButton = document.querySelector(".start-quiz");
+    var timer = document.querySelector(".timer");
+    var secondsLeft = 60;
+    var board = document.querySelector(".main-area");
 
-function displayQuestion(){
-    var questionToDisplay = document.createElement("p");
-    questionToDisplay.id = "question"
-    board.append(questionToDisplay)
-    answers = document.createElement("ul");
-    board.append(answers)
-    var wrong_answers = questons[questionCounter]["list of wrong answers"];
-    var correct_answer = questons[questionCounter]["correct answer"];
-    questionToDisplay.textContent = questons[questionCounter]["question"];
-    total_answers = wrong_answers.length + 1;
-    correct_index = Math.floor(Math.random() * total_answers);
-    wrong_answers.splice(correct_index, 0, correct_answer);
-    console.log(wrong_answers)
-   
-    for (var i = 0; i< total_answers; i++){
-        console.log(wrong_answers[i])
-        li = document.createElement("li");
-        li.id = i;
-        console.log(typeof wrong_answers[i])
-        li.textContent = wrong_answers[i];
-        answers.append(li);
+    function startTheQuiz(event){
+
+        board.innerHTML = "";
+        displayQuestion();
+
     }
 
-}
+    var correct_index;
+    var answers;
 
-function selectAnAnswer(event){
-    var element = event.target;
-    console.log(element);
-    if (element.matches("li")){ 
-       index = element.id;
-       var result = document.createElement("h3");
-       board.append(result);  
-       result.id = "result";
-       if (index == correct_index){
-            console.log('we are correct!')
-            result.textContent = 'Correct!';
-        }
-        else{
-            console.log('we are wrong!')
-            result.textContent = 'Wrong!\n 5 secons is sunbstructed now!'
-        }
+    function displayQuestion(){
+        questionCounter ++;
+        if (questions[questionCounter]== null){
+            var statementToDisplay = document.createElement("h1");
+            statementToDisplay.textContent = "YOU WON!"
+            board.append(statementToDisplay)
 
+            return;
+        }
+        var questionToDisplay = document.createElement("p");
+        questionToDisplay.id = "question"
+        board.append(questionToDisplay)
+        answers = document.createElement("ul");
+        board.append(answers)
         
-       
-    }
+        var wrong_answers = questions[questionCounter]["list of wrong answers"];
+        var correct_answer = questions[questionCounter]["correct answer"];
+        questionToDisplay.textContent = questions[questionCounter]["question"];
+        total_answers = wrong_answers.length + 1;
+        correct_index = Math.floor(Math.random() * total_answers);
+        wrong_answers.splice(correct_index, 0, correct_answer);
+        console.log(wrong_answers)
     
+        for (var i = 0; i< total_answers; i++){
+            console.log(wrong_answers[i])
+            li = document.createElement("li");
+            li.id = i;
+            console.log(typeof wrong_answers[i])
+            li.textContent = wrong_answers[i];
+            answers.append(li);
+        }
+        
+
+    }
+
+    function selectAnAnswer(event){
+        if (!questionsAnswered[questionCounter]){
+
+            var element = event.target;
+            console.log(element);
+            
+            if (element.matches("li")){ 
+                index = element.id;
+                var result = document.createElement("h3");
+                board.append(result);  
+                result.id = "result";
+                if (index == correct_index){
+                        console.log('we are correct!')
+                        result.textContent = 'Correct!';
+                    }
+                    else{
+                        console.log('we are wrong!')
+                        result.textContent = 'Wrong! 5 seconds are subtracted now!'
+                    }
+                
+                questionsAnswered[questionCounter] = true;
+                setTimeout(goToNextQuestion, 1000);
+            
+            }
+
+
+        }
+  
+
+    }
+
+    function goToNextQuestion(){
+        board.innerHTML = "";
+        displayQuestion();
+    }
+
+    board.addEventListener("click", selectAnAnswer);
+    startButton.addEventListener("click", startTheQuiz)
 
 }
-
-board.addEventListener("click", selectAnAnswer);
-startButton.addEventListener("click", startTheQuiz)
+fetchData()
